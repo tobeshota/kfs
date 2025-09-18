6f44a59c9035fa1583a5d866de6e68f4a7145a32
smizuoch <smizuoch@student.42tokyo.jp>
2025-09-18 15:27:12 +0900

    ref: main.c: remove unnecessary terminal writes and use serial_write for integration test output

--- init/main.c
+++ init/main.c
@@ -6,11 +6,7 @@ void kernel_main(void)
 	serial_init();
 	terminal_initialize();
 	/* Integration test expects '42' on the serial (COM1) output. */
-	serial_write("42\n", 3);
 	terminal_writestring("42\n");
-	terminal_writestring("Hello, kernel World!\n");
-	terminal_writestring("This is a minimal i386 kernel.\n");
-	terminal_writestring("Newline and scrolling are supported.\n");
 }
 
 /* テスト用ラッパは drivers 実装側に残存 */
