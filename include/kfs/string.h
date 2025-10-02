838d6649d21f03c7b7958a0e32f9ea8c035b2ce9
smizuoch <smizuoch@student.42tokyo.jp>
2025-10-02 18:50:47 +0900

    string.h: Remove unnecessary C++ extern block for cleaner code

--- include/kfs/string.h
+++ include/kfs/string.h
@@ -8,66 +8,57 @@ typedef size_t __kernel_size_t;
 #define __KERNEL_SIZE_T_DEFINED
 #endif
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #ifndef __HAVE_ARCH_STRCPY
-	char *strcpy(char *dst, const char *src);
+char *strcpy(char *dst, const char *src);
 #endif
 #ifndef __HAVE_ARCH_STRNCPY
-	char *strncpy(char *dst, const char *src, __kernel_size_t count);
+char *strncpy(char *dst, const char *src, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_STRLCPY
-	__kernel_size_t strlcpy(char *dst, const char *src, __kernel_size_t size);
+__kernel_size_t strlcpy(char *dst, const char *src, __kernel_size_t size);
 #endif
 #ifndef __HAVE_ARCH_STRCAT
-	char *strcat(char *dst, const char *src);
+char *strcat(char *dst, const char *src);
 #endif
 #ifndef __HAVE_ARCH_STRNCAT
-	char *strncat(char *dst, const char *src, __kernel_size_t count);
+char *strncat(char *dst, const char *src, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_STRCMP
-	int strcmp(const char *lhs, const char *rhs);
+int strcmp(const char *lhs, const char *rhs);
 #endif
 #ifndef __HAVE_ARCH_STRNCMP
-	int strncmp(const char *lhs, const char *rhs, __kernel_size_t count);
+int strncmp(const char *lhs, const char *rhs, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_STRCHR
-	char *strchr(const char *str, int ch);
+char *strchr(const char *str, int ch);
 #endif
 #ifndef __HAVE_ARCH_STRRCHR
-	char *strrchr(const char *str, int ch);
+char *strrchr(const char *str, int ch);
 #endif
 #ifndef __HAVE_ARCH_STRSTR
-	char *strstr(const char *haystack, const char *needle);
+char *strstr(const char *haystack, const char *needle);
 #endif
 #ifndef __HAVE_ARCH_STRLEN
-	__kernel_size_t strlen(const char *s);
+__kernel_size_t strlen(const char *s);
 #endif
 #ifndef __HAVE_ARCH_STRNLEN
-	__kernel_size_t strnlen(const char *s, __kernel_size_t maxlen);
+__kernel_size_t strnlen(const char *s, __kernel_size_t maxlen);
 #endif
 
 #ifndef __HAVE_ARCH_MEMSET
-	void *memset(void *dst, int value, __kernel_size_t count);
+void *memset(void *dst, int value, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_MEMCPY
-	void *memcpy(void *dst, const void *src, __kernel_size_t count);
+void *memcpy(void *dst, const void *src, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_MEMMOVE
-	void *memmove(void *dst, const void *src, __kernel_size_t count);
+void *memmove(void *dst, const void *src, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_MEMCMP
-	int memcmp(const void *lhs, const void *rhs, __kernel_size_t count);
+int memcmp(const void *lhs, const void *rhs, __kernel_size_t count);
 #endif
 #ifndef __HAVE_ARCH_MEMCHR
-	void *memchr(const void *ptr, int ch, __kernel_size_t count);
-#endif
-
-#ifdef __cplusplus
-}
+void *memchr(const void *ptr, int ch, __kernel_size_t count);
 #endif
 
 #endif /* _STRING_H_ */
