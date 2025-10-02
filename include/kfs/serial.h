074f731dfdf882f8635bc9f2080337d70f79735c
smizuoch <smizuoch@student.42tokyo.jp>
2025-10-02 18:50:10 +0900

    serial.h: Remove unnecessary C++ extern block for cleaner code

--- include/kfs/serial.h
+++ include/kfs/serial.h
@@ -5,16 +5,7 @@
 
 #include <stddef.h>
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-	void serial_init(void);
-	void serial_write(const char *data, size_t size); /* 既存実装の公開(必要最小限) */
-
-#ifdef __cplusplus
-}
-#endif
+void serial_init(void);
+void serial_write(const char *data, size_t size); /* 既存実装の公開(必要最小限) */
 
 #endif /* KFS_SERIAL_H */
