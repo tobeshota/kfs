2dd7e38f6169e5f601390b3603bec0cfdc99efa2
smizuoch <smizuoch@student.42tokyo.jp>
2025-10-02 18:49:42 +0900

    ref: io.h: Clean up function definitions and remove unnecessary preprocessor directives

--- include/asm-i386/io.h
+++ include/asm-i386/io.h
@@ -3,26 +3,18 @@
 
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C"
+/** ポートマップドI/O **/
+/* I/Oポート番号port指定のデバイスに対してvalを出力する */
+static inline void outb(uint16_t port, uint8_t val)
 {
-#endif
-	/** ポートマップドI/O **/
-	/* I/Oポート番号port指定のデバイスに対してvalを出力する */
-	static inline void outb(uint16_t port, uint8_t val)
-	{
-		__asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
-	}
-	/* I/Oポート番号port指定のデバイスに対してvalを入力する */
-	static inline uint8_t inb(uint16_t port)
-	{
-		uint8_t r;
-		__asm__ volatile("inb %1, %0" : "=a"(r) : "Nd"(port));
-		return r;
-	}
-
-#ifdef __cplusplus
+	__asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
 }
-#endif
+/* I/Oポート番号port指定のデバイスに対してvalを入力する */
+static inline uint8_t inb(uint16_t port)
+{
+	uint8_t r;
+	__asm__ volatile("inb %1, %0" : "=a"(r) : "Nd"(port));
+	return r;
+}
 
 #endif /* ASM_I386_IO_H */
