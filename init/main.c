04006737922761baef6a87ca8e5cd94bd84ff0c7
tobeshota <Shota.Tobe@keio.jp>
2025-10-23 15:22:10 +0900

    feat: make sure the first number that appears on the screen is 42

--- init/main.c
+++ init/main.c
@@ -11,6 +11,9 @@ void start_kernel(void)
 	kfs_keyboard_init();
 	kfs_terminal_set_color(kfs_vga_make_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
 
+	/* Integration test expects '42' on the serial (COM1) output. */
+	printk("42\n");
+
 	/* Debug: show segment selectors after gdt_init */
 	unsigned short cs, ds, ss;
 	asm volatile("mov %%cs, %0" : "=r"(cs)); /* 現在のCSレジスタ %%cs を C変数 cs に書き込む */
@@ -18,12 +21,10 @@ void start_kernel(void)
 	asm volatile("mov %%ss, %0" : "=r"(ss)); /* 現在のSSレジスタ %%ss を C変数 ss に書き込む */
 	printk("GDT loaded: CS=%x DS=%x SS=%x\n", cs, ds, ss);
 
-	/* Integration test expects '42' on the serial (COM1) output. */
-	printk("42\n");
-
-	/* printk("Early stack dump after kernel init banner:\n");
-	extern void dump_stack(void);
-	dump_stack(); */
+	/* 早期スタックダンプはコメントアウト（フォーマット問題のため一時的に無効化） */
+	// printk("Early stack dump after kernel init banner:\n");
+	// extern void dump_stack(void);
+	// dump_stack();
 
 	kfs_terminal_set_color(kfs_vga_make_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
 	printk("Alt+F1..F4 switch consoles; keyboard echo ready.\n");
