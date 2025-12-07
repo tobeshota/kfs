4ab30f16d99b92040a242abbc7740c5d049e581c
tobeshota <Shota.Tobe@keio.jp>
2025-12-07 22:23:02 +0900

    feat: Prepare qwerty and azerty keyboard layouts and make them switchable

--- drivers/tty/keyboard.c
+++ drivers/tty/keyboard.c
@@ -23,9 +23,13 @@ static int extended_prefix;
 /* カスタムキーボードハンドラ（シェルなどが登録する） */
 static keyboard_handler_t custom_handler = NULL;
 
+/* 現在のキーボードレイアウト */
+static kbd_layout_t current_layout = KBD_LAYOUT_QWERTY;
+
 extern uint8_t kfs_io_inb(uint16_t port);
 
-static const char scancode_map_normal[128] = {
+/* QWERTY配列（US）- 通常キー */
+static const char scancode_map_qwerty_normal[128] = {
 	[0x02] = '1',  [0x03] = '2', [0x04] = '3',	[0x05] = '4', [0x06] = '5', [0x07] = '6',  [0x08] = '7',
 	[0x09] = '8',  [0x0A] = '9', [0x0B] = '0',	[0x0C] = '-', [0x0D] = '=', [0x0F] = '\t', [0x10] = 'q',
 	[0x11] = 'w',  [0x12] = 'e', [0x13] = 'r',	[0x14] = 't', [0x15] = 'y', [0x16] = 'u',  [0x17] = 'i',
@@ -35,13 +39,35 @@ static const char scancode_map_normal[128] = {
 	[0x30] = 'b',  [0x31] = 'n', [0x32] = 'm',	[0x33] = ',', [0x34] = '.', [0x35] = '/',  [0x39] = ' ',
 };
 
-static const char scancode_map_shift[128] = {
+/* QWERTY配列（US）- Shiftキー押下時 */
+static const char scancode_map_qwerty_shift[128] = {
 	[0x02] = '!', [0x03] = '@', [0x04] = '#', [0x05] = '$', [0x06] = '%', [0x07] = '^', [0x08] = '&', [0x09] = '*',
 	[0x0A] = '(', [0x0B] = ')', [0x0C] = '_', [0x0D] = '+', [0x10] = 'Q', [0x11] = 'W', [0x12] = 'E', [0x13] = 'R',
 	[0x14] = 'T', [0x15] = 'Y', [0x16] = 'U', [0x17] = 'I', [0x18] = 'O', [0x19] = 'P', [0x1A] = '{', [0x1B] = '}',
 	[0x1E] = 'A', [0x1F] = 'S', [0x20] = 'D', [0x21] = 'F', [0x22] = 'G', [0x23] = 'H', [0x24] = 'J', [0x25] = 'K',
 	[0x26] = 'L', [0x27] = ':', [0x28] = '"', [0x29] = '~', [0x2B] = '|', [0x2C] = 'Z', [0x2D] = 'X', [0x2E] = 'C',
 	[0x2F] = 'V', [0x30] = 'B', [0x31] = 'N', [0x32] = 'M', [0x33] = '<', [0x34] = '>', [0x35] = '?',
+};
+
+/* AZERTY配列（フランス語）- 通常キー */
+static const char scancode_map_azerty_normal[128] = {
+	[0x02] = '&', [0x03] = 'e', [0x04] = '"', [0x05] = '\'', [0x06] = '(', [0x07] = '-',  [0x08] = 'e',
+	[0x09] = '_', [0x0A] = 'c', [0x0B] = 'a', [0x0C] = ')',	 [0x0D] = '=', [0x0F] = '\t', [0x10] = 'a',
+	[0x11] = 'z', [0x12] = 'e', [0x13] = 'r', [0x14] = 't',	 [0x15] = 'y', [0x16] = 'u',  [0x17] = 'i',
+	[0x18] = 'o', [0x19] = 'p', [0x1A] = '^', [0x1B] = '$',	 [0x1E] = 'q', [0x1F] = 's',  [0x20] = 'd',
+	[0x21] = 'f', [0x22] = 'g', [0x23] = 'h', [0x24] = 'j',	 [0x25] = 'k', [0x26] = 'l',  [0x27] = 'm',
+	[0x28] = 'u', [0x29] = '*', [0x2B] = '*', [0x2C] = 'w',	 [0x2D] = 'x', [0x2E] = 'c',  [0x2F] = 'v',
+	[0x30] = 'b', [0x31] = 'n', [0x32] = ',', [0x33] = ';',	 [0x34] = ':', [0x35] = '!',  [0x39] = ' ',
+};
+
+/* AZERTY配列（フランス語）- Shiftキー押下時 */
+static const char scancode_map_azerty_shift[128] = {
+	[0x02] = '1', [0x03] = '2', [0x04] = '3', [0x05] = '4', [0x06] = '5', [0x07] = '6', [0x08] = '7', [0x09] = '8',
+	[0x0A] = '9', [0x0B] = '0', [0x0C] = 'o', [0x0D] = '+', [0x10] = 'A', [0x11] = 'Z', [0x12] = 'E', [0x13] = 'R',
+	[0x14] = 'T', [0x15] = 'Y', [0x16] = 'U', [0x17] = 'I', [0x18] = 'O', [0x19] = 'P', [0x1A] = '"', [0x1B] = 'L',
+	[0x1E] = 'Q', [0x1F] = 'S', [0x20] = 'D', [0x21] = 'F', [0x22] = 'G', [0x23] = 'H', [0x24] = 'J', [0x25] = 'K',
+	[0x26] = 'L', [0x27] = 'M', [0x28] = '%', [0x29] = 'u', [0x2B] = 'u', [0x2C] = 'W', [0x2D] = 'X', [0x2E] = 'C',
+	[0x2F] = 'V', [0x30] = 'B', [0x31] = 'N', [0x32] = '?', [0x33] = '.', [0x34] = '/', [0x35] = 'S',
 };
 
 /* シフトキーが押されているかどうかを判定する */
@@ -53,7 +79,22 @@ static int shift_active(void)
 /* スキャンコードから対応するascii codeを取得する */
 static char translate_scancode(uint8_t code)
 {
-	char base = scancode_map_normal[code];
+	/* 現在のレイアウトに対応するキーマップを選択 */
+	const char *map_normal;
+	const char *map_shift;
+
+	if (current_layout == KBD_LAYOUT_AZERTY)
+	{
+		map_normal = scancode_map_azerty_normal;
+		map_shift = scancode_map_azerty_shift;
+	}
+	else
+	{
+		map_normal = scancode_map_qwerty_normal;
+		map_shift = scancode_map_qwerty_shift;
+	}
+
+	char base = map_normal[code];
 	if (!base)
 	{
 		return 0;
@@ -65,7 +106,7 @@ static char translate_scancode(uint8_t code)
 	}
 	if (shift_active())
 	{
-		char shifted = scancode_map_shift[code];
+		char shifted = map_shift[code];
 		if (shifted)
 		{
 			return shifted;
@@ -103,6 +144,23 @@ void kfs_keyboard_reset(void)
 	caps_lock = 0;
 	extended_prefix = 0;
 	custom_handler = NULL;
+	current_layout = KBD_LAYOUT_QWERTY;
+}
+
+/** キーボードレイアウトを設定する
+ * @param layout 設定するレイアウト(KBD_LAYOUT_QWERTY または KBD_LAYOUT_AZERTY)
+ */
+void kfs_keyboard_set_layout(kbd_layout_t layout)
+{
+	current_layout = layout;
+}
+
+/** 現在のキーボードレイアウトを取得する
+ * @return 現在のレイアウト
+ */
+kbd_layout_t kfs_keyboard_get_layout(void)
+{
+	return current_layout;
 }
 
 /** キーボードIRQハンドラ（IRQ1から呼ばれる）
