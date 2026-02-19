3b310ea664dedbf0b7eb960ae70a244ef7d2f3c1
tobeshota <Shota.Tobe@keio.jp>
2026-02-19 17:38:15 +0900

    chore: move Capability definitions into capability.h

--- include/kfs/sched.h
+++ include/kfs/sched.h
@@ -1,6 +1,7 @@
 #ifndef _KFS_SCHED_H
 #define _KFS_SCHED_H
 
+#include <kfs/capability.h>
 #include <kfs/list.h>
 #include <kfs/mm_types.h>
 #include <kfs/pid.h>
@@ -23,24 +24,6 @@ typedef struct
  * @note 単一CPUを前提とする場合，通常のintと同じ
  */
 typedef int refcount_t;
-
-/* POSIX Capability型 */
-typedef struct
-{
-	uint32_t cap[2]; /* Capabilityビットマスク（64ビット = u32 x 2） */
-} kernel_cap_t;
-
-/* POSIX Capability */
-#define CAP_CHOWN 0		   /* ファイル所有者変更 */
-#define CAP_DAC_OVERRIDE 1 /* DAC（任意アクセス制御）を無視 */
-#define CAP_KILL 5		   /* 任意プロセスへシグナル送信 */
-#define CAP_SETUID 7	   /* UID設定 */
-#define CAP_SYS_ADMIN 21   /* システム管理操作 */
-
-/* 全Capability有効 */
-#define CAP_FULL_SET ((kernel_cap_t){{0xffffffff, 0xffffffff}})
-/* 全Capability無効 */
-#define CAP_EMPTY_SET ((kernel_cap_t){{0, 0}})
 
 /** シグナル共有情報
  * Linux 6.18ではスレッドグループで共有されるシグナル情報
